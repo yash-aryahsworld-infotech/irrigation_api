3c6bb90ebcac93004de9cc0ce3bf5264212212ce
yash-aryahsworld-infotech <yash.aryahsworld@gmail.com>
2025-12-15 13:33:37 +0530

    Refactor WSGI configuration for Vercel compatibility and update settings module path

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.insert(0, path)
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'irrigation_project.settings')
+
+# This is the Vercel-compatible handler that runs the Django app
 application = get_wsgi_application()
